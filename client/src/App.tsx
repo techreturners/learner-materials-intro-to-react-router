eb4b618b4e2fb5f37dd03e701e04a31560a42260
Atty Cronin <antoniajcronin@gmail.com>
2023-07-29 18:04:15 +0100

    Add NotFound for nonsense url path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import Home from './components/Home';
 import Confess from './components/Confess';
 import Misdemeanour from './components/Misdemeanour';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -13,10 +13,11 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/confess' element={<Confess />} />
         <Route path='/misdemeanour' element={<Misdemeanour />} />
+        <Route path='/*' element={<NotFound />} />
       </Routes>
       <h1>Vite + React</h1>
     </>
   );
 }
 
-export default App
+export default App;
