eb3aefa16a27ee459ea94cd9214f18241eeef065
Atty Cronin <antoniajcronin@gmail.com>
2023-07-30 17:16:33 +0100

    Add semantic HTML tags to header and footer

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,11 +1,12 @@
 import Nav from './Nav';
 import Logo from './Logo';
+import './Header.css';
 
 const Header: React.FC = () => (
-  <>
+  <header className='header'>
     <Logo />
     <Nav />
-  </>
+  </header>
 );
 
 export default Header;
