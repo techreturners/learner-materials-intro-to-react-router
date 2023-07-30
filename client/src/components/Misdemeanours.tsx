0812943d09ec886528d4a999db81483e54c42953
Atty Cronin <antoniajcronin@gmail.com>
2023-07-30 19:25:36 +0100

    feat: Implement fetch function to getMisdemeanours

--- client/src/components/Misdemeanours.tsx
+++ client/src/components/Misdemeanours.tsx
@@ -1,3 +1,23 @@
-const Misdemeanours: React.FC = () => <main>Misdemeanour</main>;
+import { useEffect, useState } from 'react';
+
+import { getMisdemeanours } from '../utils/fetch';
+
+const Misdemeanours: React.FC = () => {
+  const [crimes, setCrimes] = useState({});
+  const url = 'http://localhost:8080/api/misdemeanours/';
+  const noOfMisdemeanours = 2;
+
+  useEffect(() => {
+    const data = getMisdemeanours(url, noOfMisdemeanours);
+    setCrimes(data);
+  }, []);
+
+  return (
+    <main>
+      Misdemeanours
+      {crimes && <p>we got crimes</p>}
+    </main>
+  );
+};
 
 export default Misdemeanours;
