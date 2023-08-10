52535f516fa6b149f3a9465f0138625646d109bb
Atty Cronin <antoniajcronin@gmail.com>
2023-08-10 18:56:37 +0100

    Add loading state

--- client/src/components/Misdemeanours.tsx
+++ client/src/components/Misdemeanours.tsx
@@ -1,25 +1,34 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { MisdemeanourContext } from '../utils/context';
 import Misdemeanour from './Misdemeanour';
 
 const Misdemeanours: React.FC = () => {
-  const { crimes, setAmount } = useContext(MisdemeanourContext);
+  const { crimes, setAmount, loading } = useContext(MisdemeanourContext);
 
   const { amount } = useParams();
-  amount && setAmount(amount);
+  useEffect(() => {
+    amount && setAmount(amount);
+  }, []);
 
   return (
     <>
+      {loading && (
+        <div className='text-white font-semibold text-center p-2 my-2'>
+          Loading...
+        </div>
+      )}
       <h1>Misdemeanours</h1>
       <ul className='border-2 border-red-100 table'>
-        {crimes &&
+        {!loading &&
+          crimes &&
           crimes.map((crime) => (
             <li key={crime.citizenId}>
               <Misdemeanour crime={crime} />
             </li>
           ))}
       </ul>
+      {!loading && crimes && crimes.length === 0 && <li>No results</li>}
     </>
   );
 };
