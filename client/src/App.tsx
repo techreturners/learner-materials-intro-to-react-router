79e6437203b29d8122f4d62710f529f6b08a78c3
Atty Cronin <antoniajcronin@gmail.com>
2023-08-10 16:09:21 +0100

    Refactor App and routes to use context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import MisdemeanourContextProvider from './utils/context';
+import AppRoutes from './AppRoutes';
 import './App.css';
-
-import Home from './components/Home';
-import Confess from './components/Confess';
-import Misdemeanours from './components/Misdemeanours';
-import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/confess' element={<Confess />} />
-        <Route path='/misdemeanours' element={<Misdemeanours />} />
-        <Route path='/*' element={<NotFound />} />
-      </Routes>
-    </>
+    <MisdemeanourContextProvider>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </MisdemeanourContextProvider>
   );
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import Layout from './components/Layout.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Layout>
-        <App />
-      </Layout>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
