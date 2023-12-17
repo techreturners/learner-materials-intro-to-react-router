5b3f44ec35c61fc8d2786166d658fffd5b9ec5be
netcam22 <annettelesage@btinternet.com>
2023-12-17 15:02:10 +0000

    feat: implement mild public rudeness nested route

---
+++ client/src/components/misdemeanours/misdemeanour/mild_public_rudeness.tsx
@@ -0,0 +1,8 @@
+const MildPublicRudeness = () => {
+   
+return(<section>
+    <p>Mild Public Rudeness</p>
+    <p>is a misdemeanour</p>
+</section>)}
+
+export default MildPublicRudeness;
--- client/src/components/router/router.tsx
+++ client/src/components/router/router.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../../components/home/home";
 import Confession from "../confession/confession";
 import Misdemeanours from "../misdemeanours/misdemeanours";
+import MildPublicRudeness from "../misdemeanours/misdemeanour/mild_public_rudeness";
 import MainLayout from "../../components/layouts/main_layout";
 import NotFound from "../not_found/not_found";
 
@@ -11,6 +12,7 @@ export const Router = () => {
         <Route path = "/" element = {<MainLayout/>}>
             <Route index element = {<Home/>}/>
             <Route path = "misdemeanours" element = {<Misdemeanours/>}/>
+            <Route path = "/misdemeanours/:mild_public_rudeness" element = {<MildPublicRudeness />}/>
             <Route path = "confession" element = {<Confession/>}/>
             <Route path = "*" element = {<NotFound/>}/>
         </Route>
