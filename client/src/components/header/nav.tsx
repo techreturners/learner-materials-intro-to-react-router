7a7aa3bdb5a73c3cea0ff42964322de33af730a4
netcam22 <annettelesage@btinternet.com>
2023-12-17 15:04:00 +0000

    feat: add mild public rudeness to navbar

--- client/src/components/header/nav.tsx
+++ client/src/components/header/nav.tsx
@@ -3,7 +3,11 @@ import { NavLink } from "react-router-dom";
 const Nav = () => <nav>
     <ul>
         <li><NavLink to = "/">Home</NavLink></li>
-        <li><NavLink to = "/misdemeanours">Misdemeanours</NavLink></li>
+        <li><NavLink to = "/misdemeanours">Misdemeanours</NavLink>
+            <ul>
+            <li><NavLink to = "/misdemeanours/mild_public_rudeness">Mild Public Rudeness</NavLink></li>
+            </ul>
+        </li>
         <li><NavLink to = "/confession">Confess To Us</NavLink></li>
     </ul>
 </nav>;
