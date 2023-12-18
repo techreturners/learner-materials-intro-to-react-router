eb916963efedb305fd4c580439c0ee760d715e2f
netcam22 <annettelesage@btinternet.com>
2023-12-18 22:40:54 +0000

    feat: remove border

--- client/src/components/misdemeanours/misdemeanour-container.tsx
+++ client/src/components/misdemeanours/misdemeanour-container.tsx
@@ -10,7 +10,7 @@ interface MisdemeanourResponse {
 
 const Misdemeanours : React.FC<MisdemeanoursProps> = ({url}) => {
 
-const response = useFetch<MisdemeanourResponse>(url);;
+const response = useFetch<MisdemeanourResponse>(url);
 return (
 <>
     <h2 className = "title">Misdemeanours!</h2>
