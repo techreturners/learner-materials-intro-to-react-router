a04e54cb2b0673f7da7440dabe7337622a943463
netcam22 <annettelesage@btinternet.com>
2023-12-21 14:53:16 +0000

    feat: add icon type and const and display on page

--- client/src/components/misdemeanours/misdemeanour-item.tsx
+++ client/src/components/misdemeanours/misdemeanour-item.tsx
@@ -1,11 +1,12 @@
-import {Misdemeanour} from "../../../types/misdemeanours.types";
+import {Misdemeanour, MISDEMEANOUR_ICONS} 
+from "../../../types/misdemeanours.types";
 
 const MisdemeanourItem : React.FC<Misdemeanour> = ({citizenId, misdemeanour, date}) => {
 
     return (
     <>
         <div className = "cell">Citizen Id: {citizenId}</div>
-        <div className = "cell">Misdemeanour: {misdemeanour}</div>
+        <div className = "cell">Misdemeanour: {MISDEMEANOUR_ICONS[misdemeanour]} {misdemeanour}</div>
         <div className = "cell">Date: {date}</div>
         <div className = "cell">Images goes here</div>
     </>
